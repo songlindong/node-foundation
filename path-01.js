b91f64646716594eef8c16b6fca82013ca009e50
songlindong <857344690@qq.com>
2023-03-26 11:55:57 +0800

    feat: path模块

--- path-01.js
+++ path-01.js
@@ -33,5 +33,47 @@ const path = require('path')
  */
 // console.log(path.extname(__filename))
 // console.log(path.extname('/a/b'))
-console.log(path.extname('/a/b/index.html.js.css'))
-console.log(path.extname('/a/b/index.html.js.'))
+// console.log(path.extname('/a/b/index.html.js.css'))
+// console.log(path.extname('/a/b/index.html.js.'))
+
+// 解析路径
+/**
+ * 接受一个路径，返回一个对象，包含不同的信息
+ * 
+ */
+// const obj = path.parse('/a/b/c/index.html')
+// const obj = path.parse('./a/b/c/')
+// console.log(obj)
+// console.log(obj.name)
+
+// 5、序列化路径
+// const obj = path.parse('./a/b/c/')
+// console.log(path.format(obj))
+
+// 6、判断当前路径是否为绝对路径
+
+// console.log(path.isAbsolute('foo'))
+// console.log(path.isAbsolute('/foo'))
+// console.log(path.isAbsolute('//foo'))
+// console.log(path.isAbsolute(''))
+// console.log(path.isAbsolute('.'))
+// console.log(path.isAbsolute('./bar'))
+
+ // 7、拼接路径
+//  console.log(path.join('a/b/c', 'c', 'index.html'))
+//  console.log(path.join('/a/b/c', 'c', '../', 'index.html'))
+//  console.log(path.join(''))
+
+ // 8、规范化路径
+//  console.log(path.normalize(''))
+//  console.log(path.normalize('a/b/c/d'))
+//  console.log(path.normalize('a////b/c../d'))
+//  console.log(path.normalize('a/\b/c../d'))
+
+// 9、绝对路径
+/**
+ * resolve([from], to)
+ */
+// console.log(path.resolve())
+// console.log(path.resolve('a', 'b'))
+console.log(path.resolve('index.html'))
