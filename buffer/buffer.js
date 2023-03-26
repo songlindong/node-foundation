c76efadcda3ed1641699da3d4f72701623eb797e
songlindong <857344690@qq.com>
2023-03-26 18:41:46 +0800

    feat: buffer基础

---
+++ buffer/buffer.js
@@ -0,0 +1,25 @@
+// const b1 = Buffer.alloc(10)
+// const b2 = Buffer.allocUnsafe(10)
+
+// console.log(b1)
+// console.log(b2)
+
+// from 函数
+// const b1 = Buffer.from('中')
+// console.log(b1)
+
+// const b1 = Buffer.from([0xe4, 0xb8, 0xad])
+// console.log(b1)
+
+// const b1 = Buffer.from('中')
+// console.log(b1)
+// console.log(b1.toString())
+
+const b1 = Buffer.alloc(3)
+const b2 = Buffer.from(b1)
+console.log(b1)
+console.log(b2)
+
+b1[0] = 1
+console.log(b1)
+console.log(b2)
