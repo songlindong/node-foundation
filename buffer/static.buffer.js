bd928da1ca1cc5ad332902f6a5b5a32f5f5ee013
songlindong <857344690@qq.com>
2023-03-29 00:09:35 +0800

    feat: Buffer静态方法

---
+++ buffer/static.buffer.js
@@ -0,0 +1,13 @@
+// concat
+
+// let b1 = Buffer.from('拉勾')
+// let b2 = Buffer.from('教育')
+
+// let b = Buffer.concat([b1, b2], 9)
+
+// console.log(b)
+// console.log(b.toString())
+
+// isBuffer
+let b1 = Buffer.alloc(3)
+console.log(Buffer.isBuffer(b1))
